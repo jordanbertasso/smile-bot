7b1299ae1a4676ae0096734939e55466cb805eb4
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2021-05-22 21:48:03 +1000

    Remove ctx arg from check

--- bot/util/checks.py
+++ bot/util/checks.py
@@ -1,7 +1,7 @@
 from .. import constants
 from discord.ext import commands
 
-def is_jordan(ctx):
+def is_jordan():
   def predicate(ctx):
     return ctx.message.author.id == constants.JORDAN_ID
   return commands.check(predicate)
